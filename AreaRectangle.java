0231c98926841ce8b806b8f8e1a42ca9aa7342f9
priti-manek <manek.priti@gmail.com>
2020-02-18 23:05:55 +0530

    first commit

---
+++ AreaRectangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AreaRectangle {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double width = 5.6;
+        double height = 8.5;
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+        System.out.println("Area of Rectangle : " + area);
+        System.out.println("Perimeter of rectangle:" + perimeter);
+    }
+
+}
+
+
+
+
+
+
+
